f2d82365c0dd8f77867058af557f6d59ae0633b2
Yeldos <90935343+Sholtan@users.noreply.github.com>
2024-02-26 11:58:09 +0300

    Add files via upload
    shoot photons at angle arctan(400/1800)

--- main.py
+++ main.py
@@ -46,14 +46,12 @@ def find_z(start_point, angle_to_the_axis, sphere_center, radius):
     all_x.append(x)
     all_z.append(second_intersection_point[2] + p*second_refracted_direction[2])  
 
-    return (all_x, all_z)
+    return (x, y, all_x, all_z)
 
 
-start_point = np.array([410., 0., -100.])
-#print("\nstart_point: ", start_point, "\n")
 
-angle_to_the_axis = 0.
-#print("\n angle_to_the_axis: ", angle_to_the_axis)
+angle_to_the_axis = np.arctan(400/1800)
+print("\n angle_to_the_axis in degrees: ", angle_to_the_axis*rad_to_deg)
 
 sphere_center = np.array([0, 0, 1850.])
 #print("\n sphere_center: ", sphere_center, "\n")
@@ -64,11 +62,11 @@ radius = 1900.  # in mm
 out_x = []
 out_y = []
 
-n = 200
+n = 4000
 rng = np.random.default_rng()
 
 angles = 2*np.pi * rng.random((n,))
-rs = 410* rng.random((n,))
+rs = 408* rng.random((n,))
 
 x = []
 y = []
@@ -81,15 +79,15 @@ for i in range(n):
 ax = plt.subplot()
 for i in range(n):
     zz = find_z(np.array([x[i], y[i], -100]), angle_to_the_axis, sphere_center, radius)
-    #out_x.append(zz[0])
-    #out_y.append(zz[1])
-    all_x = zz[0]
-    all_z = zz[1]
-    #print("all_x: ", all_x)
-    #print("all_z: ", all_z)
+    out_x.append(zz[0])
+    out_y.append(zz[1])
+    all_x = zz[2]
+    all_z = zz[3]
     ax.plot(all_z, all_x)
-
-
+ax.axis('equal')
+ax.set_xlabel('z [mm]', fontsize=20)
+ax.set_ylabel('x [mm]', fontsize=20)
+plt.grid(True)
 #f0, ax0 = plt.subplots()
 #ax0.hist(out_x, bins=200)
 
@@ -97,13 +95,15 @@ for i in range(n):
 #f1, ax1 = plt.subplots()
 #ax1.hist(out_y, bins=200)
 
+f1, ax1 = plt.subplots()
+ax1.scatter(out_x, out_y)
+ax1.axis('equal')
+ax1.set_xlabel('x [mm]', fontsize=20)
+ax1.set_ylabel('y [mm]', fontsize=20)
+plt.grid(True)
+plt.show()
 
-#ax.scatter(out_x, out_y)
 
 
 
-
-ax.axis('equal')
-ax.set_xlabel('z [mm]', fontsize=20)
-ax.set_ylabel('x [mm]', fontsize=20)
 plt.show()
